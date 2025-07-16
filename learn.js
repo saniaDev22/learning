4d49bf445dc18bd1686872bf74e01ba586233e6d
saniadev22 <saniadev2222@gmail.com>
2025-07-16 15:11:16 +0530

    initial setup

---
+++ learn.js
@@ -0,0 +1,50 @@
+// variable and data types
+
+
+// const Nameof="Sania";
+// let email="Saniarwat2222@gmail.com";
+// let city="patiala";
+// let State="punjab"; 
+// var no= 1;
+
+
+
+// console.table([Nameof,city,State])
+// var age=25;
+// if (true){
+//     console.log(age)
+// }
+
+
+
+// loops.....
+
+
+// nameprint 10 times
+
+// for (let i=1;i<=10;i++)
+// {
+//     console.log(i)
+// }
+
+//  now reverse 10 Time
+
+// for(let i=10;i>=0;i--)
+// {
+//     console.log(i)
+// }
+
+// break
+for (let i=1;i<=6;i++)
+{
+    if (i==4)
+    {
+        break;
+    }
+       else{
+            console.log(i);
+        }
+}
+const name= (()=>{
+console.log("sania")
+})
